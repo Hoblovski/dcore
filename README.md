107ee32788b224281537692449b3b1a40b889ddb
Hoblovski <daizy19@mails.tsinghua.edu.cn>
2022-10-12 22:39:47 +0800

    Initial commit: baremetal boot
    Follows rcore-tutorial ch1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dev
+依赖
+```
+rustup component add llvm-tools-preview
+rustup target add riscv64gc-unknown-none-elf
+```
+
+使用 vscode：安装 rust-analyzer，并且把设置写到 .vscode/settings.json
---
+++ kernel/src/lang_items.rs
@@ -0,0 +1,18 @@
+//! Items required by the Rust language.
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "[kernel] Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("[kernel] Panicked: {}", info.message().unwrap());
+    }
+    loop {}
+}
